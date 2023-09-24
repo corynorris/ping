accf545ab359e591f306e2bf5a937b48307045dc
cory <corynorris@gmail.com>
2023-09-24 15:14:22 -0400

    made things pretty

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const escapeHtml = require('escape-html');
 const helmet = require('helmet');
 const cors = require('cors');
 const rateLimit = require('express-rate-limit');
@@ -36,77 +35,75 @@ app.use((req, res, next) => {
 app.set('view engine', 'ejs');
 
 
-const MAX_MESSAGES = 50;
 let messages = [];
+const MAX_MESSAGE_LENGTH = 2048;
+const MAX_MESSAGES = 10;
 
-app.get('/', (req, res) => {
-  res.render('index.ejs');
-});
+function addMessage( method, message, ip, debug = {}) {
+  const timestamp = new Date();
+  const escapedMessage = message.substring(0, MAX_MESSAGE_LENGTH);
 
-app.all('/ping', (req, res) => {
-  let message;
+  const newMessage = {timestamp, method, message: escapedMessage,  ip, debug };
 
-  if (req.method === 'POST') {
-    message = req.body.message;
-  } else if (req.method === 'GET') {
-    message = req.query.message;
+  messages.unshift(newMessage);
+
+  while (messages.length > MAX_MESSAGES) {
+    messages.pop()
   }
 
+  io.emit('ping',  newMessage);
+
+}
+
+app.post('/ping', (req, res) => {
+  const message = JSON.stringify(req.body, null, 2);
+
   if (message) {
-    // Limit message to 100 characters
-    message = message.substring(0, 100);
-
-    // Escape malicious content
-    const escapedMessage = escapeHtml(message);
-
-    // Add message to the list with timestamp and IP address
-    const timestamp = new Date();
     const ip = req.ip;
-    messages.push({ message: escapedMessage, timestamp, ip });
 
-    // Keep only the last 50 messages
-    if (messages.length > MAX_MESSAGES) {
-      messages = messages.slice(messages.length - MAX_MESSAGES);
-    }
-
-    io.emit('ping', { message: escapedMessage, timestamp, ip });
+    addMessage('POST', message, ip);
 
     res.sendStatus(200);
   } else {
-    res.sendStatus(400); // Bad request if no message provided
+    res.sendStatus(400); 
   }
 });
+
+app.get('/ping', (req, res) => {
+  const message = new URLSearchParams(req.query).toString()
+  
+  if (message) {
+    const ip = req.ip;
+
+    addMessage('GET', message, ip);
+
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(400); 
+  }
+});
+
 
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  // Send the last 50 messages to the client on connection
   socket.emit('init', messages);
-
-  // Send IP address and timestamp to the client on connection
-  const ip = socket.handshake.address;
-  const timestamp = new Date();
-  socket.emit('connectionInfo', { ip, timestamp });
 
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
 
   socket.on('ping', (message) => {
-    const escapedMessage = escapeHtml(message);
-
-    // Add message to the list with timestamp and IP address
-    const timestamp = new Date();
     const ip = socket.handshake.address;
-    messages.push({ message: escapedMessage, timestamp, ip });
-
-    // Keep only the last 50 messages
-    if (messages.length > MAX_MESSAGES) {
-      messages = messages.slice(messages.length - MAX_MESSAGES);
-    }
-
-    io.emit('ping', { message: escapedMessage, timestamp, ip });
+  
+    addMessage('SOCKET', message, ip);
   });
+});
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
 });
 
 const port = process.env.PORT || 3000;
